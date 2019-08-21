01f95e4260f2e51b6fa1ea9af1ae96eecccec675
qiaogw <qiaogw@gmail.com>
2019-08-21 17:59:09 +0800

    bundled-plugins

--- sonarqube/entrypoint.sh
+++ sonarqube/entrypoint.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 set -e
 chown -R 1000 "$SONARQUBE_HOME"
+chown -R 1000 "$SONARQUBE_HOME/conf"
+chown -R 1000 "$SONARQUBE_HOME/data"
+chown -R 1000 "$SONARQUBE_HOME/bundled-plugins"
+chown -R 1000 "$SONARQUBE_HOME/extensions"
+chown -R 1000 "$SONARQUBE_HOME/logs"
 pwd
 exec gosu sonarqube ./bin/run.sh
