39154068c5b08b8ea4bca81342ab8117fd5c5c39
qiaogw <qiaogw@gmail.com>
2019-08-27 19:49:14 +0800

    pgbakup

--- pgbackrest/entrypoint.sh
+++ pgbackrest/entrypoint.sh
@@ -178,7 +178,7 @@ sed -ri "s/^#max_wal_senders\s+.*/max_wal_senders = 3/" $PGDATA/postgresql.conf
 echo "[demo]" >> /etc/pgbackrest/pgbackrest.conf  
 echo "pg1-path=$PGDATA" >> /etc/pgbackrest/pgbackrest.conf  
 echo "[global]" >> /etc/pgbackrest/pgbackrest.conf  
-echo " repo1-path=/var/lib/pgbackrest" >> /etc/pgbackrest/pgbackrest.conf
+echo " repo1-path=/pgbakup" >> /etc/pgbackrest/pgbackrest.conf
 echo " repo1-retention-full=2" >> /etc/pgbackrest/pgbackrest.conf  
 echo "[global:archive-push]" >> /etc/pgbackrest/pgbackrest.conf  
 echo "compress-level=3" >> /etc/pgbackrest/pgbackrest.conf  
