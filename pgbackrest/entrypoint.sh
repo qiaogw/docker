73c01c84286e592fce1caef84c4ba44eb7648cc7
qiaogw <qiaogw@gmail.com>
2019-08-28 13:30:41 +0800

    echo echo 'edit postgresql.conf' echo

--- pgbackrest/entrypoint.sh
+++ pgbackrest/entrypoint.sh
@@ -8,7 +8,9 @@ chmod 750 "$PGBACK"
 
 
 source /docker-entrypoint.sh #引入postgres脚本
-
+echo
+echo 'edit pgbackrest.conf'
+echo
 echo "[demo]" >> /etc/pgbackrest/pgbackrest.conf  
 echo "pg1-path=$PGDATA" >> /etc/pgbackrest/pgbackrest.conf  
 echo "[global]" >> /etc/pgbackrest/pgbackrest.conf  
@@ -16,7 +18,9 @@ echo " repo1-path=$PGBACK" >> /etc/pgbackrest/pgbackrest.conf
 echo " repo1-retention-full=2" >> /etc/pgbackrest/pgbackrest.conf  
 echo "[global:archive-push]" >> /etc/pgbackrest/pgbackrest.conf  
 echo "compress-level=3" >> /etc/pgbackrest/pgbackrest.conf  
-
+echo
+echo 'edit postgresql.conf'
+echo
 sed -ri 's/^#wal_level\s+.*/wal_level = replica/' $PGDATA/postgresql.conf 
 sed -ri "s/^#archive_command\s+.*/archive_command = 'pgbackrest --stanza=demo archive-push %p'/" $PGDATA/postgresql.conf 
 sed -ri "s/^#archive_mode\s+.*/archive_mode = on/" $PGDATA/postgresql.conf 
@@ -25,7 +29,9 @@ sed -ri "s/^#max_wal_senders\s+.*/max_wal_senders = 3/" $PGDATA/postgresql.conf
 
 
 
-
+echo
+echo ' conf complated'
+echo
 
 
 exec "$@"
