0d80efa6cae021b9c427ffe5588b5666dfcd5cfc
qiaogw <qiaogw@gmail.com>
2019-08-28 19:03:46 +0800

    cnd

--- pgbackrest/entrypoint.sh
+++ pgbackrest/entrypoint.sh
@@ -165,6 +165,28 @@ if [ "$1" = 'postgres' ]; then
 		echo
 		echo 'PostgreSQL init process complete; ready for start up.'
 		echo
+		mkdir -p "$PGBACK" 
+		chmod 750 "$PGBACK"
+		chown -R postgres  "$PGBACK"
+		mkdir -p "$PGDATA/log"
+		chmod 770 "$PGDATA/log"
+		chown -R postgres  "$PGDATA/log"
+		
+		sed -ri 's/^#wal_level\s+.*/wal_level = replica/' $PGDATA/postgresql.conf 
+        sed -ri "s/^#archive_command\s+.*/archive_command = 'pgbackrest --stanza=demo archive-push %p'/" $PGDATA/postgresql.conf 
+        sed -ri "s/^#archive_mode\s+.*/archive_mode = on/" $PGDATA/postgresql.conf 
+        sed -ri "s/^#log_line_prefix\s+.*/log_line_prefix = ''/" $PGDATA/postgresql.conf
+        sed -ri "s/^#max_wal_senders\s+.*/max_wal_senders = 3/" $PGDATA/postgresql.conf  
+
+		cat /dev/null >  /etc/pgbackrest/pgbackrest.conf  
+		echo "[demo]" >> /etc/pgbackrest/pgbackrest.conf  
+		echo "pg1-path=$PGDATA" >> /etc/pgbackrest/pgbackrest.conf  
+		echo "[global]" >> /etc/pgbackrest/pgbackrest.conf  
+		echo " repo1-path=$PGBACK" >> /etc/pgbackrest/pgbackrest.conf
+		echo " repo1-retention-full=2" >> /etc/pgbackrest/pgbackrest.conf  
+		echo "[global:archive-push]" >> /etc/pgbackrest/pgbackrest.conf  
+		echo "compress-level=3" >> /etc/pgbackrest/pgbackrest.conf  
+
 	fi
 fi
 
