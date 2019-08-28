d6f7e6e91f0da249f3033cb0d670ee7190066abc
qiaogw <qiaogw@gmail.com>
2019-08-28 19:01:10 +0800

    f

--- pgbackrest/entrypoint.sh
+++ pgbackrest/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-
 set -Eeo pipefail
-
-
-
 # TODO swap to -Eeuo pipefail above (after handling all potentially-unset variables)
 
 # usage: file_env VAR [DEFAULT]
@@ -169,32 +165,7 @@ if [ "$1" = 'postgres' ]; then
 		echo
 		echo 'PostgreSQL init process complete; ready for start up.'
 		echo
-		mkdir -p "$PGBACK" 
-		chmod 750 "$PGBACK"
-		chown -R postgres  "$PGBACK"
-		mkdir -p "$PGDATA/log"
-		chmod 770 "$PGDATA/log"
-		chown -R postgres  "$PGDATA/log"
-
-		cat /dev/null >  /etc/pgbackrest/pgbackrest.conf  
-		echo "[demo]" >> /etc/pgbackrest/pgbackrest.conf  
-		echo "pg1-path=$PGDATA" >> /etc/pgbackrest/pgbackrest.conf  
-		echo "[global]" >> /etc/pgbackrest/pgbackrest.conf  
-		echo " repo1-path=$PGBACK" >> /etc/pgbackrest/pgbackrest.conf
-		echo " repo1-retention-full=2" >> /etc/pgbackrest/pgbackrest.conf  
-		echo "[global:archive-push]" >> /etc/pgbackrest/pgbackrest.conf  
-		echo "compress-level=3" >> /etc/pgbackrest/pgbackrest.conf  
-		
-		sed -ri 's/^#wal_level\s+.*/wal_level = replica/' $PGDATA/postgresql.conf 
-        sed -ri "s/^#archive_command\s+.*/archive_command = 'pgbackrest --stanza=demo archive-push %p'/" $PGDATA/postgresql.conf 
-        sed -ri "s/^#archive_mode\s+.*/archive_mode = on/" $PGDATA/postgresql.conf 
-        sed -ri "s/^#log_line_prefix\s+.*/log_line_prefix = ''/" $PGDATA/postgresql.conf
-        sed -ri "s/^#max_wal_senders\s+.*/max_wal_senders = 3/" $PGDATA/postgresql.conf  
-
 	fi
 fi
 
 exec "$@"
-
-
-
