2e9add6d3ccd8b7f656d3455ae5fb297cebc89d0
qiaogw <qiaogw@gmail.com>
2019-08-28 19:11:52 +0800

    fd

--- pgbackrest/entrypoint.sh
+++ pgbackrest/entrypoint.sh
@@ -185,12 +185,7 @@ if [ "$1" = 'postgres' ]; then
 		echo "[global:archive-push]" >> /etc/pgbackrest/pgbackrest.conf  
 		echo "compress-level=3" >> /etc/pgbackrest/pgbackrest.conf  
 		LOGDIR="$PGDATA/log"
-		sed -ri 's/^#logging_collector\s+.*/logging_collector = on  /' $PGDATA/postgresql.conf 
-        sed -ri "s/^#log_directory\s+.*/log_directory = '$LOGDIR'/" $PGDATA/postgresql.conf 
-        sed -ri "s/^#log_filename\s+.*/log_filename = 'postgresql-%a.log'/" $PGDATA/postgresql.conf 
-        sed -ri "s/^#log_truncate_on_rotation\s+.*/log_truncate_on_rotation = on/" $PGDATA/postgresql.conf
-        sed -ri "s/^#log_rataion_age\s+.*/log_rataion_age = 1d/" $PGDATA/postgresql.conf
-        sed -ri "s/^#log_ratation_size\s+.*/log_ratation_size = 0/" $PGDATA/postgresql.conf
+
 		mkdir -p "$LOGDIR"
 		chmod 770 "$LOGDIR"
 		chown -R postgres  "$LOGDIR"
