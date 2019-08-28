f1b4a9573d596c94628e2b010a30d68589b8c024
qiaogw <qiaogw@gmail.com>
2019-08-28 18:01:01 +0800

    LOGDIR

--- pgbackrest/entrypoint.sh
+++ pgbackrest/entrypoint.sh
@@ -174,10 +174,11 @@ if [ "$1" = 'postgres' ]; then
 		echo 'PostgreSQL init process complete; ready for start up.'
 		echo
 
-        mkdir -p "$PGDATA/log"
-        chmod 770 "$PGDATA/log"
-        chown -R postgres  "$PGDATA/log"
 
+        LOGDIR="$PGDATA/log"
+        mkdir -p "$LOGDIR"
+        chmod 770 "$LOGDIR"
+        chown -R postgres  "$LOGDIR"
         sed -ri 's/^#wal_level\s+.*/wal_level = replica/' $PGDATA/postgresql.conf 
         sed -ri "s/^#archive_command\s+.*/archive_command = 'pgbackrest --stanza=demo archive-push %p'/" $PGDATA/postgresql.conf 
         sed -ri "s/^#archive_mode\s+.*/archive_mode = on/" $PGDATA/postgresql.conf 
@@ -185,7 +186,7 @@ if [ "$1" = 'postgres' ]; then
         sed -ri "s/^#max_wal_senders\s+.*/max_wal_senders = 3/" $PGDATA/postgresql.conf  
 
         sed -ri 's/^#logging_collector\s+.*/logging_collector = on  /' $PGDATA/postgresql.conf 
-        sed -ri "s/^#log_directory\s+.*/log_directory = '$PGDATA/log'/" $PGDATA/postgresql.conf 
+        sed -ri "s/^#log_directory\s+.*/log_directory = '$LOGDIR'/" $PGDATA/postgresql.conf 
         sed -ri "s/^#log_filename\s+.*/log_filename = 'postgresql-%a.log'/" $PGDATA/postgresql.conf 
         sed -ri "s/^#log_truncate_on_rotation\s+.*/log_truncate_on_rotation = on/" $PGDATA/postgresql.conf
         sed -ri "s/^#log_rataion_age\s+.*/log_rataion_age = 1d/" $PGDATA/postgresql.conf
