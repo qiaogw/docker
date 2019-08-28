cd247238a7c5489828c6ed4a22f38de52cbccc3f
qiaogw <qiaogw@gmail.com>
2019-08-28 13:21:49 +0800

    $PGDATA

--- pgbackrest/entrypoint.sh
+++ pgbackrest/entrypoint.sh
@@ -6,6 +6,9 @@ mkdir -p "$PGBACK"
 chown -R postgres  "$PGBACK"
 chmod 750 "$PGBACK"
 
+
+source /docker-entrypoint.sh #引入postgres脚本
+
 echo "[demo]" >> /etc/pgbackrest/pgbackrest.conf  
 echo "pg1-path=$PGDATA" >> /etc/pgbackrest/pgbackrest.conf  
 echo "[global]" >> /etc/pgbackrest/pgbackrest.conf  
@@ -14,7 +17,6 @@ echo " repo1-retention-full=2" >> /etc/pgbackrest/pgbackrest.conf
 echo "[global:archive-push]" >> /etc/pgbackrest/pgbackrest.conf  
 echo "compress-level=3" >> /etc/pgbackrest/pgbackrest.conf  
 
-source /docker-entrypoint.sh #引入postgres脚本
 sed -ri 's/^#wal_level\s+.*/wal_level = replica/' $PGDATA/postgresql.conf 
 sed -ri "s/^#archive_command\s+.*/archive_command = 'pgbackrest --stanza=demo archive-push %p'/" $PGDATA/postgresql.conf 
 sed -ri "s/^#archive_mode\s+.*/archive_mode = on/" $PGDATA/postgresql.conf 
