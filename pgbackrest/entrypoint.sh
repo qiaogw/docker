d6ea7157209181b85076e7e31ffc4436d0d6c399
qiaogw <qiaogw@gmail.com>
2019-08-28 18:09:42 +0800

    pgbackrest --stanza=demo --log-level-console=info stanza-create

--- pgbackrest/entrypoint.sh
+++ pgbackrest/entrypoint.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
 set -Eeo pipefail
+function trap_sigterm() {
+    echo_warn "Signal trap triggered, beginning shutdown.." >> $PGDATA/trap.output
+    echo_warn "Signal trap triggered, beginning shutdown.."
+
+    # Clean shutdowns begin here (force fast mode in case of PostgreSQL < 9.5)
+    echo_info "Cleanly shutting down PostgreSQL in force fast mode.."
+    pg_ctl -w -D $PGDATA -m fast stop
+
+    # Unclean shutdowns begin here (if all else fails)
+    if [ -f $PGDATA/postmaster.pid ]; then
+            kill -SIGINT $(head -1 $PGDATA/postmaster.pid) >> $PGDATA/trap.output
+    fi
+    if [[ ${ENABLE_SSHD} == "true" ]]; then
+        echo_info "killing SSHD.."
+        killall sshd
+    fi
+}
+
+trap 'trap_sigterm' SIGINT SIGTERM
 
 whoami
 mkdir -p "$PGBACK" 
@@ -201,7 +220,18 @@ echo " repo1-retention-full=2" >> /etc/pgbackrest/pgbackrest.conf
 echo "[global:archive-push]" >> /etc/pgbackrest/pgbackrest.conf  
 echo "compress-level=3" >> /etc/pgbackrest/pgbackrest.conf  
 
+
+echo_info "Starting PostgreSQL.."
+postgres -D $PGDATA 
+
 pgbackrest --stanza=demo --log-level-console=info stanza-create
 
 
-exec "$@"
+
+# exec "$@"
+# We will wait indefinitely until "docker stop [container_id]"
+# When that happens, we route to the "trap_sigterm" function above
+wait
+
+echo_info "PostgreSQL is shutting down. Exiting.."
+
