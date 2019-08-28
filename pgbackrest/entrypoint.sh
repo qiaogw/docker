a42fa7fc5756ed3e6294de6407a407e2d27044a8
qiaogw <qiaogw@gmail.com>
2019-08-28 19:16:01 +0800

    fd

--- pgbackrest/entrypoint.sh
+++ pgbackrest/entrypoint.sh
@@ -189,13 +189,15 @@ if [ "$1" = 'postgres' ]; then
 		mkdir -p "$LOGDIR"
 		chmod 770 "$LOGDIR"
 		chown -R postgres  "$LOGDIR"
+
+	fi
+fi
+
 sed -ri 's/^#logging_collector\s+.*/logging_collector = on  /' $PGDATA/postgresql.conf 
 sed -ri "s/^#log_directory\s+.*/log_directory = '$LOGDIR'/" $PGDATA/postgresql.conf 
 sed -ri "s/^#log_filename\s+.*/log_filename = 'postgresql-%a.log'/" $PGDATA/postgresql.conf 
 sed -ri "s/^#log_truncate_on_rotation\s+.*/log_truncate_on_rotation = on/" $PGDATA/postgresql.conf
 sed -ri "s/^#log_rataion_age\s+.*/log_rataion_age = 1d/" $PGDATA/postgresql.conf
 sed -ri "s/^#log_ratation_size\s+.*/log_ratation_size = 0/" $PGDATA/postgresql.conf
-	fi
-fi
 
 exec "$@"
