1262e9633983644682d85f9f07684fb46a25ba2e
qiaogw <qiaogw@gmail.com>
2019-08-29 16:49:58 +0800

    tt

--- pgbackrest/entrypoint.sh
+++ pgbackrest/entrypoint.sh
@@ -115,4 +115,28 @@ if [ -z "$(ls -A "$PGDATA")" ]; then
 	echo 'PostgreSQL init process complete; ready for start up.'
 fi
 
+
+
+function trap_sigterm() {
+    echo "Signal trap triggered, beginning shutdown.." >> $PGDATA/trap.output
+    echo "Signal trap triggered, beginning shutdown.."
+
+    # Clean shutdowns begin here (force fast mode in case of PostgreSQL < 9.5)
+    echo "Cleanly shutting down PostgreSQL in force fast mode.."
+    gosu postgres pg_ctl -w -D $PGDATA -m fast stop
+
+    # Unclean shutdowns begin here (if all else fails)
+    if [ -f $PGDATA/postmaster.pid ]; then
+            kill -SIGINT $(head -1 $PGDATA/postmaster.pid) >> $PGDATA/trap.output
+    fi
+    if [[ ${ENABLE_SSHD} == "true" ]]; then
+        echo "killing SSHD.."
+        killall sshd
+    fi
+}
+#trap 'trap_sigterm' SIGKILL SIGTERM SIGHUP SIGINT EXIT
+trap "echo stopping by SIGTERM" SIGTERM
+trap "echo stopping by SIGINT" SIGINT
+trap "echo stopping by SIGKILL" SIGKILL
+
 exec   "$@"
