ff64bbc1b0c01708f0db1a0569f30567a5e2386c
qiaogw <qiaogw@gmail.com>
2019-08-29 16:16:16 +0800

    echo_info

--- pgbackrest/start.sh
+++ pgbackrest/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 function trap_sigterm() {
-    echo_warn "Signal trap triggered, beginning shutdown.." >> $PGDATA/trap.output
-    echo_warn "Signal trap triggered, beginning shutdown.."
+    echo "Signal trap triggered, beginning shutdown.." >> $PGDATA/trap.output
+    echo "Signal trap triggered, beginning shutdown.."
 
     # Clean shutdowns begin here (force fast mode in case of PostgreSQL < 9.5)
-    echo_info "Cleanly shutting down PostgreSQL in force fast mode.."
+    echo "Cleanly shutting down PostgreSQL in force fast mode.."
     gosu postgres pg_ctl -w -D $PGDATA -m fast stop
 
     # Unclean shutdowns begin here (if all else fails)
@@ -12,7 +12,7 @@ function trap_sigterm() {
             kill -SIGINT $(head -1 $PGDATA/postmaster.pid) >> $PGDATA/trap.output
     fi
     if [[ ${ENABLE_SSHD} == "true" ]]; then
-        echo_info "killing SSHD.."
+        echo "killing SSHD.."
         killall sshd
     fi
 }
@@ -24,6 +24,6 @@ gosu postgres postgres
 
 # We will wait indefinitely until "docker stop [container_id]"
 # When that happens, we route to the "trap_sigterm" function above
-wait
+wait $!
 
-echo_info "PostgreSQL is shutting down. Exiting.."
+echo "PostgreSQL is shutting down. Exiting.."
